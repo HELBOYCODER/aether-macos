427ee1cd87cd6fbb7cab29f80b006a472269959e
Hermes <hermes@nous>
2026-08-15 04:32:09 +0000

    fix: @main on AppDelegate, drop main.swift

--- AetherApp/Sources/AppDelegate.swift
+++ AetherApp/Sources/AppDelegate.swift
@@ -1,12 +1,15 @@
 import AppKit
 import SwiftUI
 
+@main
 final class AppDelegate: NSObject, NSApplicationDelegate {
     private let menu = MenuBarController()
     private let manager = AetherManager.shared
 
     func applicationDidFinishLaunching(_ notification: Notification) {
+        NSApp.setActivationPolicy(.accessory)  // menu-bar only, no Dock icon
         menu.build()
+        OnboardingWindow.shared.showIfNeeded()
         manager.startOnLaunchIfEnabled()
     }
 

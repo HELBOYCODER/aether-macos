dd0dfb7883e703150ced3d5d338a054ee6d545af
HELBOY CODER <quilt-that-refract@duck.com>
2026-09-18 07:21:47 -0700

    Expose packet socket HEV bridge API

--- AetherApp/PacketTunnel/HEVBridge.h
+++ AetherApp/PacketTunnel/HEVBridge.h
@@ -1,17 +1,12 @@
 #ifndef AETHER_HEV_BRIDGE_H
 #define AETHER_HEV_BRIDGE_H
-
 #include <stddef.h>
-
 #ifdef __cplusplus
 extern "C" {
 #endif
-
-int aether_hev_start(const char *config, size_t config_len, int tun_fd);
+int aether_hev_start(const char *config, size_t config_len);
 void aether_hev_stop(void);
-
 #ifdef __cplusplus
 }
 #endif
-
 #endif

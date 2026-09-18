3ac8a3fefd9536aa72175c71df10a2fdeb46a92e
HELBOY CODER <quilt-that-refract@duck.com>
2026-09-18 07:29:29 -0700

    Clear stale upstream bypass state on VPN stop

--- AetherApp/Sources/PacketTunnelManager.swift
+++ AetherApp/Sources/PacketTunnelManager.swift
@@ -148,6 +148,7 @@ final class PacketTunnelManager: ObservableObject {
     }
 
     func stop() {
+        pendingBypassIPs = []
         manager?.connection.stopVPNTunnel()
         status = manager?.connection.status ?? .disconnected
     }

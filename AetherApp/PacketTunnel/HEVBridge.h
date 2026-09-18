620f082f8d4c7a2a3170eaed3a6ba3c43b03ea66
HELBOY CODER <quilt-that-refract@duck.com>
2026-09-18 07:26:42 -0700

    Use the real Network Extension utun descriptor for HEV

--- AetherApp/PacketTunnel/HEVBridge.h
+++ AetherApp/PacketTunnel/HEVBridge.h
@@ -4,7 +4,7 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-int aether_hev_start(const char *config, size_t config_len);
+int aether_hev_start(const char *config, size_t config_len, int tun_fd);
 void aether_hev_stop(void);
 #ifdef __cplusplus
 }
